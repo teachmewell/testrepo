bcf29967cb92bd3142d483482d4212290a4da37c
teachmewell <pnynjn@gmail.com>
2026-06-28 01:24:16 +0200

    Update 2048.c

--- 2048.c
+++ 2048.c
@@ -1,3 +1,54 @@
+#include <stdin.h>
+#include <stdlib.h>
+
+//所有C 程序都从int main() 开始
 int main(){
-  printf("abc");
+
+//设一个array
+int field[]= {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+
+// print 
+printf("enter one of: s, x, z, y");
+  
+// 得到输入  上下左右
+char text= scanf("%c", text);
+
+  
+}
+
+
+void change(int[] field,  char input){
+  if(input == 's'){
+    int num = 16;
+    while( num > 4){
+      
+    }
+  }
+    
+ else if(input == 'x'){
+    
+  }
+   
+  else if(input == 'z'){
+    
+  }
+    
+  else if(input == 'y'){
+    
+  }
+  
+}
+
+void addElement(int[] field){
+  
+}
+
+void display(int* field){
+    for(int a = 0; i < 4; a=a+4){
+      printf(field[a]);
+      prinff(field[a+1]);
+      printf(field[a+2]);
+      prinff(field[a+3]);
+      printf("\n");
+    }
 }
