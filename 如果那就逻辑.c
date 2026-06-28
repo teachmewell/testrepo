3fd1fd3dd85829d2fd27f4244bf2ab56666072ab
teachmewell <pnynjn@gmail.com>
2026-06-28 22:13:07 +0200

    Create 如果那就逻辑.c

---
+++ 如果那就逻辑.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// 语法: if( 如果 ) { 那就 }
+
+// 为了查看两个数字是否是一样的，可以用  == 。
+// 还有 <, >, <= , >= 
+
+//比如:  if( a == b ) {int c = a+b;}
+
+// c机器里面会把 == 翻译成 0 或 1 。
+
+int main(){
+
+  int a = 1; 
+  int b = 2; 
+  
+  //所以这都行 : 
+  if( a == 1) { printf(" a is 1 "); }
+  if( a < b) { printf(" a smaller b "); }
+  if( a < (2+1) ) { printf(" a smaller 2 "); }
+
+
+  
+}
