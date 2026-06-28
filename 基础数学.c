3f98b976a188cd63e202c66645ccab457f9a6e2a
teachmewell <pnynjn@gmail.com>
2026-06-28 22:03:59 +0200

    Create 基础数学.c

---
+++ 基础数学.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+你的电脑当然也可以执行基础数学。
+
+比如  1 + 1; 
+
+但仅 1+1 后电脑不知道要把数据存到哪里。所以我们都把他出的结果存到一个变量里面。
+int a; (declare)
+a = 1+1; (assign)
+
+*/
+
+int main(){
+// TODO : 建立两个 int 变量 a 和 b 。 把 a-b 存到 c 里
+
+
+  printf("%i", a);
+  printf("%i", b);
+  printf("%i", c);
+}
